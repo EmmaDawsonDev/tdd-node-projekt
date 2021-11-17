cc1b1862eb78b016ee4ebe00f385d5454f8b9f8d
PelleLindroth <pellelindroth@gmail.com>
2021-11-17 12:05:48 +0100

    Implement GET and POST routes for products

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,14 +1,34 @@
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
+import { productsDb, IProduct } from '../database/products'
 
 const router = Router()
 
-router.get('/', (req, res) => {
-  res.send('Hi!')
+router.get('/', (req: Request, res: Response) => {
+  res.status(200).json(productsDb)
 })
 
-router.get('/:id')
+router.get('/:id', (req: Request, res: Response) => {
+  const { id } = req.params
 
-router.post('/')
+  const product = productsDb.find(product => product.id === id)
+
+  if (product) {
+    res.status(200).json(product)
+  } else {
+    res.status(404).json({ message: `Product with id ${id} not found` })
+  }
+})
+
+router.post('/', (req: Request, res: Response) => {
+  const product: IProduct = req.body.product
+  
+  if (product.id && product.name && product.price) {
+    productsDb.push(product)
+    res.status(201).json({ product })
+  } else {
+    res.status(400).json({ message: 'Invalid product'})
+  }
+})
 
 router.put('/:id')
 
